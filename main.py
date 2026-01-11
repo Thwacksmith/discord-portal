591428308c64e8de914008a3a7404efb49423002
Thwacksmith <thwacksmith@gmail.com>
2026-01-10 19:44:44 -0600

    message editing and deleting

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ from discord import Thread
 from discord.abc import GuildChannel, PrivateChannel
 
 DEFAULT_WEBHOOK_NAME = 'Portal'
+MAX_MESSAGE_HISTORY = 5
 
 async def send_message(webhook, message):
-    await webhook.send(
+    return await webhook.send(
         content = message.content,
+        wait = True,
         username = message.author.name,
         avatar_url = message.author.avatar.url,
         files = [await x.to_file() for x in message.attachments]
     )
+
+async def edit_message(before, after):
+    return await before.edit(
+        content = after.content,
+        attachments = after.attachments
+    )
+
+async def delete_message(message):
+    return await message.delete()
 
 class PortalBot(discord.Client):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.portals = []
         self.channel_id_set = set()
+        self.message_history = []
 
     async def on_ready(self) -> None:
         with open('config.json') as file:
@@ -71,8 +83,50 @@ class PortalBot(discord.Client):
             return
 
         webhooks = [x[1] for x in self.portals]
+        sent_messages = []
+
         for webhook in webhooks[:index] + webhooks[index+1:]:
-            await send_message(webhook, message)
+            sent_message = await send_message(webhook, message)
+            if sent_message is not None:
+                sent_messages.append(sent_message)
+
+        self.message_history.append((message.id, sent_messages))
+        if len(self.message_history) > MAX_MESSAGE_HISTORY:
+            self.message_history = self.message_history[:1]
+
+    async def on_message_edit(self, before, after):
+        if before.author.bot:
+            return
+
+        if before.channel.id not in self.channel_id_set:
+            return
+
+        try:
+            index = [x[0] for x in self.message_history].index(before.id)
+        except ValueError:
+            return
+
+        sent_messages = [x[1] for x in self.message_history][index]
+        for message in sent_messages:
+            await edit_message(message, after)
+
+    async def on_message_delete(self, message):
+        if message.author.bot:
+            return
+
+        if message.channel.id not in self.channel_id_set:
+            return
+
+        try:
+            index = [x[0] for x in self.message_history].index(message.id)
+        except ValueError:
+            return
+
+        sent_messages = [x[1] for x in self.message_history][index]
+        for message in sent_messages:
+            await delete_message(message)
+
+        del self.message_history[index]
 
 with open('config.json') as file:
     data = json.load(file)
