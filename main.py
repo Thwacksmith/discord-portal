7441f4944ac768dee8135761963cb4b40becb186
Thwacksmith <thwacksmith@gmail.com>
2026-01-11 01:51:13 -0600

    replaced tuple lists with dictionaries

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord import Thread
 from discord.abc import GuildChannel, PrivateChannel
 
 DEFAULT_WEBHOOK_NAME = 'Portal'
-MAX_MESSAGE_HISTORY = 5
+MAX_MESSAGE_HISTORY = 100
 
 async def send_message(webhook, message):
     return await webhook.send(
@@ -28,8 +28,7 @@ async def delete_message(message):
 class PortalBot(discord.Client):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.portals = []
-        self.channel_id_set = set()
+        self.portals = {}
         self.message_history = []
 
     async def on_ready(self) -> None:
@@ -61,31 +60,22 @@ class PortalBot(discord.Client):
                 print('Creating webhook for this channel')
                 webhook = await channel.create_webhook(name = DEFAULT_WEBHOOK_NAME)
 
-            self.portals.append((channel, webhook))
+            print(f'{channel.guild.name}: #{channel.name}')
+            self.portals[channel_id] = webhook
 
-        self.channel_id_set = set([x[0].id for x in self.portals])
-
-        print(f'Portal count: {len(self.portals)}')
-        for portal in self.portals:
-            print(f'{portal[0].guild.name}: #{portal[0].name}')
         print('Ready')
 
     async def on_message(self, message) -> None:
         if message.author.bot:
             return
 
-        if message.channel.id not in self.channel_id_set:
+        if message.channel.id not in self.portals.keys():
             return
 
-        try:
-            index = [x[0] for x in self.portals].index(message.channel)
-        except ValueError:
-            return
-
-        webhooks = [x[1] for x in self.portals]
         sent_messages = []
 
-        for webhook in webhooks[:index] + webhooks[index+1:]:
+        webhooks = [y for x, y in self.portals.items() if x != message.channel.id]
+        for webhook in webhooks:
             sent_message = await send_message(webhook, message)
             if sent_message is not None:
                 sent_messages.append(sent_message)
@@ -98,7 +88,7 @@ class PortalBot(discord.Client):
         if before.author.bot:
             return
 
-        if before.channel.id not in self.channel_id_set:
+        if before.channel.id not in self.portals.keys():
             return
 
         try:
@@ -114,7 +104,7 @@ class PortalBot(discord.Client):
         if message.author.bot:
             return
 
-        if message.channel.id not in self.channel_id_set:
+        if message.channel.id not in self.portals.keys():
             return
 
         try:
